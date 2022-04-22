e3fe204a9d7da4b370c09cc0c49d10697451351e
weiyuqiang <17610722805@163.com>
2022-04-22 22:19:21 +0800

    上传OCR证件

--- epoch-loan-workshop-account/src/main/java/com/epoch/loan/workshop/account/service/UserServiceImpl.java
+++ epoch-loan-workshop-account/src/main/java/com/epoch/loan/workshop/account/service/UserServiceImpl.java
@@ -629,6 +629,8 @@ public class UserServiceImpl extends BaseService implements UserService {
             }
         }
 
+        // TODO 与风控交互，获取 证件编号与用户的 curp是否 通过
+
         // 上传证件正面图片
         String frontPath = BusinessNameUtils.createUserIdTypeFileName(NameField.USR_ID, user.getUserInfoId(), NameField.FRONT_IMAGE_TYPE);
         String frontImageUrl = OssFileUtils.uploadFileAndGetUrl(userFileBucketName, frontPath, convertToFile(params.getIdFrontImgData()), null);
@@ -655,9 +657,6 @@ public class UserServiceImpl extends BaseService implements UserService {
             result.setMessage(ResultEnum.KYC_UPLOAD_FILE_ERROR.message());
             return result;
         }
-
-        // TODO 与风控交互，获取 证件编号与用户的 curp是否 通过
-
 
         // 保存 图片信息
         userInfoById.setUserFileBucketName(userFileBucketName);
