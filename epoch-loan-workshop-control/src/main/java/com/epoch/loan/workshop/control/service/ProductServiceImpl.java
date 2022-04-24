0680e018690513745d3264b27a8e6ffb195e2a00
shangkunfeng <shangkunfeng@duandai.com>
2022-04-24 20:31:54 +0800

    调试

--- epoch-loan-workshop-control/src/main/java/com/epoch/loan/workshop/control/service/ProductServiceImpl.java
+++ epoch-loan-workshop-control/src/main/java/com/epoch/loan/workshop/control/service/ProductServiceImpl.java
@@ -91,13 +91,12 @@ public class ProductServiceImpl extends BaseService implements ProductService {
 
         }
 
-        // 认证状态
-        PlatformUserAuthEntity userAuth = platformUserAuthDao.findUserAuth(params.getUser().getId());
+        User user = params.getUser();
 
         // 封装认证状态
-        resData.setIdFlag("30".equals(userAuth.getIdCardState()) ? 1 : 0);
-        resData.setBaseInfoFlag("30".equals(userAuth.getBaseInfoState()) ? 1 : 0);
-        resData.setAddInfoFlag("30".equals(userAuth.getOtherInfoState()) ? 1 : 0);
+        resData.setIdFlag(StringUtils.isNotEmpty(user.getPapersId()) ? 1 : 0);
+        resData.setBaseInfoFlag(StringUtils.isNotEmpty(user.getMonthlyIncome()) ? 1 : 0);
+        resData.setAddInfoFlag(StringUtils.isNotEmpty(user.getChildrenNumber()) ? 1 : 0);
         resData.setOrderNo(loanOrderEntity.getId());
 
         return result;
