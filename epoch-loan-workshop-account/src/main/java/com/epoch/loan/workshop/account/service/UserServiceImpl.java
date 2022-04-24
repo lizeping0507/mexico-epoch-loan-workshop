cbfc512846350e7cfa2a0e85787a35398727c313
weiyuqiang <17610722805@163.com>
2022-04-24 18:28:24 +0800

    修改 loan_user 表时， 更新redis缓存

--- epoch-loan-workshop-account/src/main/java/com/epoch/loan/workshop/account/service/UserServiceImpl.java
+++ epoch-loan-workshop-account/src/main/java/com/epoch/loan/workshop/account/service/UserServiceImpl.java
@@ -673,6 +673,11 @@ public class UserServiceImpl extends BaseService implements UserService {
         }
         loanUserInfoDao.update(userInfoById);
 
+        // TODO 更新用户缓存
+        updateUserCache(user.getId());
+
+        // TODO 更新用户认证信息
+
         // 封装结果
         result.setReturnCode(ResultEnum.SUCCESS.code());
         result.setMessage(ResultEnum.SUCCESS.message());
