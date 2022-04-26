55479f91502871937b296beae3cd7fe2103d4ffc
shangkunfeng <shangkunfeng@duandai.com>
2022-04-26 15:40:37 +0800

    调试

--- epoch-loan-workshop-control/src/main/java/com/epoch/loan/workshop/control/service/ProductServiceImpl.java
+++ epoch-loan-workshop-control/src/main/java/com/epoch/loan/workshop/control/service/ProductServiceImpl.java
@@ -481,6 +481,7 @@ public class ProductServiceImpl extends BaseService implements ProductService {
         }
 
         // 组装列表
+        LogUtil.sysInfo("list 1:  {}", list);
         list.addAll(waitRepaymentOrderProductList);
         list.addAll(reloanOrderProductList);
         list.addAll(examinePassOrderProductList);
@@ -489,6 +490,7 @@ public class ProductServiceImpl extends BaseService implements ProductService {
         list.addAll(examineWaitOrWaitPayOrderProductList);
         list.addAll(closeProductList);
         list.addAll(examineFailOrderProductList);
+        LogUtil.sysInfo("list 2:  {}", list);
 
         LogUtil.sysInfo("waitRepaymentOrderProductList:  {}", waitRepaymentOrderProductList);
         LogUtil.sysInfo("reloanOrderProductList:  {}", reloanOrderProductList);
