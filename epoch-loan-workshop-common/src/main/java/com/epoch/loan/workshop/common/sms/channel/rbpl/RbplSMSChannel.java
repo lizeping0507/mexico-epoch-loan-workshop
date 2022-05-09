156e886f2ae5930e9d7424c82a253cd5945ab8ed
Shangkunfeng <shangkunfeng@wgleam.com>
2022-05-09 17:14:35 +0800

    调试

--- epoch-loan-workshop-common/src/main/java/com/epoch/loan/workshop/common/sms/channel/rbpl/RbplSMSChannel.java
+++ epoch-loan-workshop-common/src/main/java/com/epoch/loan/workshop/common/sms/channel/rbpl/RbplSMSChannel.java
@@ -80,7 +80,7 @@ public class RbplSMSChannel implements SMSChannel {
         params.put("callbackUrl", callbackUrl);
 
         // 发送请求
-        String result = HttpUtils.POST(url, params);
+        String result = HttpUtils.POST(url, JSONObject.toJSONString(params));
         LogUtil.sysInfo("请求发送验证码  请求参数：{} ，响应参数：{}",JSONObject.toJSONString(params), result);
 
         // 封装结果就
