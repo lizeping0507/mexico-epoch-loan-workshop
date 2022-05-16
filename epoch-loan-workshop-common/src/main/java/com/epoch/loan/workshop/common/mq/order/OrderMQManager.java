489c1b34d49ac6f7ec9a1a7e343929b095168ab8
Shangkunfeng <shangkunfeng@wgleam.com>
2022-05-16 15:59:04 +0800

    调试

--- epoch-loan-workshop-common/src/main/java/com/epoch/loan/workshop/common/mq/order/OrderMQManager.java
+++ epoch-loan-workshop-common/src/main/java/com/epoch/loan/workshop/common/mq/order/OrderMQManager.java
@@ -202,7 +202,7 @@ public class OrderMQManager extends BaseMQ {
 
                         // 未达到指定时间
                         if (delayMQParams.getDelayTime() + delayMQParams.getTime() * 1000 > System.currentTimeMillis()) {
-                            LogUtil.sysError("时间不满足:" + delayMQParams.getDelayTime() + delayMQParams.getTime() * 1000 + "|" + System.currentTimeMillis());
+                            LogUtil.sysError("时间不满足:" + (delayMQParams.getDelayTime() + delayMQParams.getTime() * 1000) + "|" + System.currentTimeMillis());
                             // 加入延时队列继续等待
                             // 消息体
                             Message msg = new Message();
