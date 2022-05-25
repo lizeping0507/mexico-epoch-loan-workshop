43249b6caa9a591e46e6d317e1b7df7cff3f6d70
zeping.li <lizeping123>
2022-05-25 16:17:32 +0800

    删除无效引用

--- epoch-loan-workshop-timing/src/main/java/com/epoch/loan/workshop/timing/task/OrderRepaymentDistributionTask.java
+++ epoch-loan-workshop-timing/src/main/java/com/epoch/loan/workshop/timing/task/OrderRepaymentDistributionTask.java
@@ -39,7 +39,7 @@ public class OrderRepaymentDistributionTask extends BaseTask implements Job {
                 // 发送还款策略组分配队列计算还款策略
                 DistributionRepaymentParams distributionRepaymentParams = new DistributionRepaymentParams();
                 distributionRepaymentParams.setOrderId(loanOrderBillEntity.getOrderId());
-                distributionRepaymentParams.setOrderBillId(loanOrderBillEntity.getOrderId());
+                distributionRepaymentParams.setOrderBillId(loanOrderBillEntity.getId());
                 repaymentMQManager.sendMessage(distributionRepaymentParams, repaymentMQManager.getDistributionSubExpression());
             } catch (Exception e) {
                 LogUtil.sysError("[OrderRepaymentDistributionTask]", e);
@@ -55,7 +55,7 @@ public class OrderRepaymentDistributionTask extends BaseTask implements Job {
                 // 发送还款策略组分配队列计算还款策略
                 DistributionRepaymentParams distributionRepaymentParams = new DistributionRepaymentParams();
                 distributionRepaymentParams.setOrderId(loanOrderBillEntity.getOrderId());
-                distributionRepaymentParams.setOrderBillId(loanOrderBillEntity.getOrderId());
+                distributionRepaymentParams.setOrderBillId(loanOrderBillEntity.getId());
                 repaymentMQManager.sendMessage(distributionRepaymentParams, repaymentMQManager.getDistributionSubExpression());
             } catch (Exception e) {
                 LogUtil.sysError("[OrderRepaymentDistributionTask]", e);
