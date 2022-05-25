a81868ecbb5d83c581e657f32ac86d7e718157b3
zeping.li <lizeping123>
2022-05-25 17:49:36 +0800

    删除无效引用

--- epoch-loan-workshop-common/src/main/java/com/epoch/loan/workshop/common/sms/SMSManager.java
+++ epoch-loan-workshop-common/src/main/java/com/epoch/loan/workshop/common/sms/SMSManager.java
@@ -42,9 +42,7 @@ public class SMSManager {
         String code = generateVerificationCode();
 
         // 发送短信验证码
-        Situation situation = smsChannel().sendVerificationCode(code,
-                "0052" + mobile);
-        LogUtil.sysInfo("发送短信验证码 : {}",situation);
+        Situation situation = smsChannel().sendVerificationCode(code, "0052" + mobile);
         if (!situation.isResult()) {
             LogUtil.sysError("[SMSManager sendVerificationCode]"+ situation.getResponse());
             return null;
