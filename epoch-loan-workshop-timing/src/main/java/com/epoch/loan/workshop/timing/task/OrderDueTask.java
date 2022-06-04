164ad1f46ccfc4a167d45842a0f05288ceac0250
zeping.li <lizeping123>
2022-06-05 03:17:08 +0800

    删除无效引用

--- epoch-loan-workshop-timing/src/main/java/com/epoch/loan/workshop/timing/task/OrderDueTask.java
+++ epoch-loan-workshop-timing/src/main/java/com/epoch/loan/workshop/timing/task/OrderDueTask.java
@@ -5,12 +5,14 @@ import com.epoch.loan.workshop.common.constant.RedisKeyField;
 import com.epoch.loan.workshop.common.entity.mysql.LoanOrderBillEntity;
 import com.epoch.loan.workshop.common.mq.order.params.OrderParams;
 import com.epoch.loan.workshop.common.util.LogUtil;
+import org.apache.commons.collections.CollectionUtils;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -35,49 +37,46 @@ public class OrderDueTask extends BaseTask implements Job {
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
         // 查询所有在途已逾期的订单
         List<LoanOrderBillEntity> loanOrderBillEntityListWay = loanOrderBillDao.findOrderBillByStatusAndRepaymentTime(OrderBillStatus.WAY, new Date());
-        loanOrderBillEntityListWay.parallelStream().forEach(loanOrderBillEntity -> {
-            try {
-                // 订单ID
-                String orderId = loanOrderBillEntity.getOrderId();
-
-                // 订单账单ID
-                String orderBillId = loanOrderBillEntity.getId();
-
-                // 增加逾期计算标识
-                redisClient.set(RedisKeyField.ORDER_BILL_DUE_LOCK + orderId, orderBillId);
-
-                OrderParams params = new OrderParams();
-                params.setGroupName("SYSTEM");
-                params.setOrderId(orderId);
-                params.setOrderBillId(orderBillId);
-                orderMQManager.sendMessage(params, orderMQManager.getOrderDueSubExpression());
-            } catch (Exception e) {
-                LogUtil.sysError("[OrderDueTask]", e);
-            }
-        });
-
 
         // 查询所有已经逾期的订单
         List<LoanOrderBillEntity> loanOrderBillEntityListDue = loanOrderBillDao.findOrderBillByStatus(OrderBillStatus.DUE);
-        loanOrderBillEntityListDue.parallelStream().forEach(loanOrderBillEntity -> {
-            try {
-                // 订单ID
-                String orderId = loanOrderBillEntity.getOrderId();
 
-                // 订单账单ID
-                String orderBillId = loanOrderBillEntity.getId();
+        // 需要计算的逾期订单
+        List<LoanOrderBillEntity> loanOrderBillEntityList = new ArrayList<>();
+        loanOrderBillEntityList.addAll(loanOrderBillEntityListWay);
+        loanOrderBillEntityList.addAll(loanOrderBillEntityListDue);
 
-                // 增加逾期计算标识
-                redisClient.set(RedisKeyField.ORDER_BILL_DUE_LOCK + orderId, orderBillId);
-
-                OrderParams params = new OrderParams();
-                params.setGroupName("SYSTEM");
-                params.setOrderId(loanOrderBillEntity.getOrderId());
-                params.setOrderBillId(loanOrderBillEntity.getId());
-                orderMQManager.sendMessage(params, orderMQManager.getOrderDueSubExpression());
-            } catch (Exception e) {
-                LogUtil.sysError("[OrderDueTask]", e);
+        // 必须将所有订单处理完
+        while (true) {
+            // 集合为空退出循环
+            if (CollectionUtils.isEmpty(loanOrderBillEntityList)) {
+                break;
             }
-        });
+
+            // 循环插入队列计算逾期
+            for (LoanOrderBillEntity loanOrderBillEntity : loanOrderBillEntityList) {
+                try {
+                    // 订单ID
+                    String orderId = loanOrderBillEntity.getOrderId();
+
+                    // 订单账单ID
+                    String orderBillId = loanOrderBillEntity.getId();
+
+                    OrderParams params = new OrderParams();
+                    params.setGroupName("SYSTEM");
+                    params.setOrderId(orderId);
+                    params.setOrderBillId(orderBillId);
+                    orderMQManager.sendMessage(params, orderMQManager.getOrderDueSubExpression());
+
+                    // 增加逾期计算标识
+                    redisClient.set(RedisKeyField.ORDER_BILL_DUE_LOCK + orderId, orderBillId);
+
+                    // 删除数据
+                    loanOrderBillEntityList.remove(loanOrderBillEntity);
+                } catch (Exception e) {
+                    LogUtil.sysError("[OrderDueTask]", e);
+                }
+            }
+        }
     }
 }
